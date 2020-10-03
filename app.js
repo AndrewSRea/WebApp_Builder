7e3378b1c2cdd28e3cc35d8112d89806df0bdbfe
AndrewSRea <andrew.s.rea@gmail.com>
2020-10-03 09:23:36 -0700

    Adding revisions

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // Eventually, we'll add some code here
+
+/* Andrews-MacBook-Air:WebApp_Builder andrewrea$ git branch -M main
+Andrews-MacBook-Air:WebApp_Builder andrewrea$ git remote add origin git@github.com:AndrewsRea/WebApp_Builder.git
+Andrews-MacBook-Air:WebApp_Builder andrewrea$ git push -u origin main */
+
+$('.intro').css('color', 'red');
+
+$('li').parent().css('background', 'purple');
