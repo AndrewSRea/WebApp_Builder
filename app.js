9c69591eba1253e95f401b3f506876bea37ffc5b
AndrewSRea <andrew.s.rea@gmail.com>
2020-10-08 09:02:06 -0700

    New revisions

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-// Eventually, we'll add some code here
-
-/* Andrews-MacBook-Air:WebApp_Builder andrewrea$ git branch -M main
-Andrews-MacBook-Air:WebApp_Builder andrewrea$ git remote add origin git@github.com:AndrewsRea/WebApp_Builder.git
-Andrews-MacBook-Air:WebApp_Builder andrewrea$ git push -u origin main */
-
 $('.intro').css('color', 'red');
 
 $('.shopping-list').css('background', 'lightblue');
 $('.shopping-list').append('<li>Oranges</li>');
+$('.shopping-list').prepend('<li>Coffee</li>');
 
-$('.add-btn').on('click', function(e) {
-    $('.shopping-list').append(input.value);
-})
+$('.add-btn').click(function(e) {
+    const inputValue = $('.add-to-list').val();
+    $('.shopping-list').append(`<li>${inputValue}</li>`);     // ('<li>${inputValue}</li>') is the jQuery version of JavaScript
+});                                                           // string concatenation: ('<li>' + inputValue + '</li>')
